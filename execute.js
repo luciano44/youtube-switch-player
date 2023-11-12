7c5592889956ba41203345e348ce782df7103b7e
luciano44 <luciano.ar6@gmail.com>
2023-11-12 03:09:36 -0300

    Prevent action button from working when user is not on youtube "shorts" page

--- execute.js
+++ execute.js
@@ -3,6 +3,11 @@
 ;(function () {
   const href = window.location.href
   const hrefSplit = href.split("/")
+
+  // Check if website is YouTube and it's on "Shorts"
+  if (!hrefSplit[2].includes("www.youtube.com")) return
+  if (!hrefSplit[3].includes("shorts")) return
+
   const videoId = hrefSplit[hrefSplit.length - 1]
 
   window.location.replace(`https://www.youtube.com/watch?v=${videoId}`)
